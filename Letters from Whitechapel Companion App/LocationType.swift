7f7137d2311df1c10c47e92ea3f732540f115fcc
bonuelc <bonuelc@gmail.com>
2017-05-31 16:07:02 -0600

    Add protocol LocationType: class
    Classes of this type will represent the nodes of the graph

---
+++ Letters from Whitechapel Companion App/LocationType.swift
@@ -0,0 +1,12 @@
+//
+//  LocationType.swift
+//  Letters from Whitechapel Companion App
+//
+//  Created by Christopher Bonuel on 5/31/17.
+//  Copyright © 2017 Christopher Bonuel. All rights reserved.
+//
+
+import Foundation
+
+protocol LocationType: class {
+}
