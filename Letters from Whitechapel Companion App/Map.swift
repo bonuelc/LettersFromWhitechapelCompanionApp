99f6155fe19d7387639b1e6b6225ae079dd0dab2
bonuelc <bonuelc@gmail.com>
2017-05-31 16:21:43 -0600

    Add Singleton class Map

---
+++ Letters from Whitechapel Companion App/Map.swift
@@ -0,0 +1,19 @@
+//
+//  Map.swift
+//  Letters from Whitechapel Companion App
+//
+//  Created by Christopher Bonuel on 5/31/17.
+//  Copyright © 2017 Christopher Bonuel. All rights reserved.
+//
+
+import Foundation
+
+// Singleton class
+class Map {
+    
+    // MARK: - Properties
+    static let shared = Map()
+    
+    // MARK: - Init
+    private init() {}
+}
