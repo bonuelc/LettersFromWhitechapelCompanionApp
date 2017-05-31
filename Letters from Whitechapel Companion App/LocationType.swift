bd9c4a489ec6a42881f2310f39d9d7303b93a77b
bonuelc <bonuelc@gmail.com>
2017-05-31 16:38:34 -0600

    Add default implementation for add method

--- Letters from Whitechapel Companion App/LocationType.swift
+++ Letters from Whitechapel Companion App/LocationType.swift
@@ -19,3 +19,15 @@ protocol LocationType: class {
     // MARK: - Methods
     func add(neighbor: LocationType)
 }
+
+// MARK: - Helper Methods
+extension LocationType {
+    func add(neighbor: LocationType) {
+        // to guard against human error in manual map building
+        precondition(self !== neighbor)
+        
+        // a.add(neighbor: b) is equivalent to b.add(neighbor: a)
+        neighbors.append(neighbor)
+        neighbor.neighbors.append(self)
+    }
+}
