9a3d4bdcb727e19b4ce86110ed6cd4291c60b3aa
bonuelc <bonuelc@gmail.com>
2017-06-01 10:52:50 -0600

    Add Unit Test target

---
+++ LettersFromWhitechapelCompanionAppTests/LettersFromWhitechapelCompanionAppTests.swift
@@ -0,0 +1,12 @@
+//
+//  LettersFromWhitechapelCompanionAppTests.swift
+//  LettersFromWhitechapelCompanionAppTests
+//
+//  Created by Christopher Bonuel on 6/1/17.
+//  Copyright © 2017 Christopher Bonuel. All rights reserved.
+//
+
+import XCTest
+
+class LettersFromWhitechapelCompanionAppTests: XCTestCase {
+}
