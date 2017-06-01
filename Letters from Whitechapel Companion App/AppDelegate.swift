92f7901db3ee91e50695fc019035d901c81b8771
bonuelc <bonuelc@gmail.com>
2017-06-01 09:23:25 -0600

    DEBUG: print Map on launch

--- Letters from Whitechapel Companion App/AppDelegate.swift
+++ Letters from Whitechapel Companion App/AppDelegate.swift
@@ -10,5 +10,9 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
+        print(Map.shared)
+        return true
+    }
 }
 
